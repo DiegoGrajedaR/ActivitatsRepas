c6b075405d7331bb80cab284e15839f90c636846
DiegoGrajedaR <diego.grajeda.7e8@itb.cat>
2025-01-08 19:41:11 +0100

    Added function "Seconds2Time" to library

---
+++ MyLibrary/MyMath.cs
@@ -0,0 +1,14 @@
+﻿namespace MyLibrary
+{
+    public class MyMath
+    {
+        public static string SecondsToTime(int totalSeconds)
+        {
+            int hours = totalSeconds / 3600;
+            int minutes = (totalSeconds % 3600) / 60;
+            int seconds = totalSeconds % 60;
+
+            return $"{hours} hores {minutes} minuts {seconds} segons";
+        }
+    }
+}
